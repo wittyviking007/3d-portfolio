8605cc848667170be1c141f4fe55ebe0246765a5
wittyviking007 <madhusnethra0205@gmail.com>
2023-09-18 17:09:30 +0530

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             window.scrollTo(0,0);
           }
         }>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={logo} alt="logo" className="w-9 h-9 rounded-full object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             Sankar Madhavan <span className="sm: block hidden ">| Web Developer</span>
           </p>
