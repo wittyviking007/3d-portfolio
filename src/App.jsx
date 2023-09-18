06e49558316acfd29532ed1160ba24e44d8cd462
wittyviking007 <madhusnethra0205@gmail.com>
2023-09-18 17:57:41 +0530

    navbar corrected

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      <Navbar />
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
           <Hero />
         </div>
       </div>
